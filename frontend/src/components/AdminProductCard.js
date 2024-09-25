ee563545fdfd29cb0bc3558b7ab5566ebe26f37d
Emna Othmen <emna.othmen@gmail.com>
2024-09-25 20:29:06 +0100

    v6

--- frontend/src/components/AdminProductCard.js
+++ frontend/src/components/AdminProductCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { MdModeEditOutline } from "react-icons/md";
 import AdminEditProduct from './AdminEditProduct';
 import displayINRCurrency from '../helpers/displayCurrency';
@@ -7,42 +7,45 @@ const AdminProductCard = ({
     data,
     fetchdata
 }) => {
-    const [editProduct,setEditProduct] = useState(false)
+    const [editProduct, setEditProduct] = useState(false);
 
-  return (
-    <div className='bg-white p-4 rounded '>
-       <div className='w-40'>
-            <div className='w-32 h-32 flex justify-center items-center'>
-              <img src={data?.productImage[0]}  className='mx-auto object-fill h-full'/>   
-            </div> 
-            <h1 className='text-ellipsis line-clamp-2'>{data.productName}</h1>
-
-            <div>
-
-                <p className='font-semibold'>
-                  {
-                    displayINRCurrency(data.sellingPrice)
-                  }
-        
-                </p>
-
-                <div className='w-fit ml-auto p-2 bg-green-100 hover:bg-green-600 rounded-full hover:text-white cursor-pointer' onClick={()=>setEditProduct(true)}>
-                    <MdModeEditOutline/>
+    return (
+        <div className='bg-gray-100 p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out'>
+            <div className='w-40 mx-auto'>
+                <div className='w-32 h-32 flex justify-center items-center mb-4'>
+                    <img
+                        src={data?.productImage[0]}
+                        alt={data.productName}
+                        className='mx-auto object-cover h-full w-full rounded-lg shadow-md transition-transform hover:scale-105 duration-300'
+                    />
                 </div>
+                <h1 className='text-lg font-semibold text-gray-800 text-center line-clamp-2 mb-2'>
+                    {data.productName}
+                </h1>
 
+                <div className='text-center'>
+                    <p className='font-semibold text-gold text-lg mb-2'>
+                        {displayINRCurrency(data.sellingPrice)}
+                    </p>
+
+                    <div
+                        className='w-fit mx-auto p-2 bg-gold hover:bg-gold-dark rounded-full hover:text-white cursor-pointer transition-colors duration-300'
+                        onClick={() => setEditProduct(true)}
+                    >
+                        <MdModeEditOutline size={20} />
+                    </div>
+                </div>
             </div>
 
-          
-       </div>
-        
-        {
-          editProduct && (
-            <AdminEditProduct productData={data} onClose={()=>setEditProduct(false)} fetchdata={fetchdata}/>
-          )
-        }
-    
-    </div>
-  )
-}
+            {editProduct && (
+                <AdminEditProduct
+                    productData={data}
+                    onClose={() => setEditProduct(false)}
+                    fetchdata={fetchdata}
+                />
+            )}
+        </div>
+    );
+};
 
-export default AdminProductCard
+export default AdminProductCard;
