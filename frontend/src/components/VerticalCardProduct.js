7ef821a9de550b8c16d6431fc4512184f1958fe2
Emna Othmen <emna.othmen@gmail.com>
2024-10-05 16:39:42 +0100

    v27

--- frontend/src/components/VerticalCardProduct.js
+++ frontend/src/components/VerticalCardProduct.js
@@ -78,16 +78,16 @@ const VerticalCardProduct = ({ category, heading }) => {
         <div className="absolute left-0 right-0 flex justify-between top-[45%] transform -translate-y-1/2 z-10">
           <button
             onClick={scrollLeft}
-            className="w-12 h-12 p-2 bg-white rounded-full shadow-lg hover:bg-gray-200 flex items-center justify-center"
+            className="w-14 h-14 p-2 bg-white rounded-full shadow-lg hover:bg-gray-200 flex items-center justify-center"
           >
-            <FaAngleLeft className="text-2xl" />
+            <FaAngleLeft className="text-3xl" />
           </button>
 
           <button
             onClick={scrollRight}
-            className="w-12 h-12 p-2 bg-white rounded-full shadow-lg hover:bg-gray-200 flex items-center justify-center"
+            className="w-14 h-14 p-2 bg-white rounded-full shadow-lg hover:bg-gray-200 flex items-center justify-center"
           >
-            <FaAngleRight className="text-2xl" />
+            <FaAngleRight className="text-3xl" />
           </button>
         </div>
       )}
@@ -100,9 +100,9 @@ const VerticalCardProduct = ({ category, heading }) => {
           ? loadingList.map((_, index) => (
               <div
                 key={index}
-                className="w-full min-w-[240px] md:min-w-[280px] max-w-[240px] md:max-w-[280px] bg-white rounded-lg flex flex-col"
+                className="w-full min-w-[260px] md:min-w-[300px] max-w-[260px] md:max-w-[300px] bg-white rounded-lg flex flex-col"
               >
-                <div className="bg-gray-100 h-72 p-4 flex justify-center items-center animate-pulse rounded-lg"></div>
+                <div className="bg-gray-100 h-80 p-4 flex justify-center items-center animate-pulse rounded-lg"></div>
                 <div className="p-4 grid gap-3 flex-grow">
                   <h2 className="font-medium text-base md:text-lg text-black p-1 py-2 animate-pulse rounded-full bg-gray-200"></h2>
                   <p className="capitalize text-gray-600 p-1 animate-pulse rounded-full bg-gray-200 py-2"></p>
@@ -110,7 +110,7 @@ const VerticalCardProduct = ({ category, heading }) => {
                     <p className="text-gray-600 font-medium p-1 animate-pulse rounded-full bg-gray-200 w-full py-2"></p>
                     <p className="text-gray-500 line-through p-1 animate-pulse rounded-full bg-gray-200 w-full py-2"></p>
                   </div>
-                  <button className="text-sm text-white px-3 rounded-full bg-gray-200 py-2 animate-pulse"></button>
+                  <button className="text-sm text-white px-4 rounded-full bg-gray-200 py-2 animate-pulse"></button>
                 </div>
               </div>
             ))
@@ -118,10 +118,10 @@ const VerticalCardProduct = ({ category, heading }) => {
               <Link
                 key={index}
                 to={"product/" + product?._id}
-                className="w-full min-w-[240px] md:min-w-[280px] max-w-[240px] md:max-w-[280px] bg-white rounded-lg flex flex-col"
+                className="w-full min-w-[260px] md:min-w-[300px] max-w-[260px] md:max-w-[300px] bg-white rounded-lg flex flex-col"
               >
                 <div
-                  className="bg-gray-100 h-72 p-4 flex justify-center items-center relative rounded-lg group"
+                  className="bg-gray-100 h-80 p-4 flex justify-center items-center relative rounded-lg group"
                   onMouseEnter={() => {
                     !isMobile && setHoveredIndex(index);
                   }}
@@ -136,7 +136,7 @@ const VerticalCardProduct = ({ category, heading }) => {
                   )}
                   <img
                     src={product.productImage[0]}
-                    className={`object-cover h-72 w-72 mx-auto rounded-lg transition-transform duration-300 ease-in-out ${
+                    className={`object-cover h-80 w-80 mx-auto rounded-lg transition-transform duration-300 ease-in-out ${
                       hoveredIndex === index ? "scale-110" : ""
                     }`}
                     alt={product.productName}
@@ -144,7 +144,7 @@ const VerticalCardProduct = ({ category, heading }) => {
                   {(isMobile || hoveredIndex === index) && (
                     <div className="absolute bottom-2 left-2 opacity-100 transition-opacity duration-200">
                       <IoBagAdd
-                        className="text-4xl text-gold hover:text-gold-dark transition-colors cursor-pointer"
+                        className="text-5xl text-gold hover:text-gold-dark transition-colors cursor-pointer"
                         onClick={(e) => handleAddToCart(e, product?._id)}
                       />
                     </div>
@@ -163,11 +163,16 @@ const VerticalCardProduct = ({ category, heading }) => {
                   {/* Ajout des étoiles d'évaluation et du nombre d'évaluations */}
                   <div className="flex justify-center items-center my-1">
                     {[...Array(5)].map((_, starIndex) => (
-                      <span key={starIndex} className={`${getStarColor(starIndex)} text-xl`}>
+                      <span
+                        key={starIndex}
+                        className={`${getStarColor(starIndex)} text-xl`}
+                      >
                         ★
                       </span>
                     ))}
-                    <span className="text-gray-600 ml-2">{fixedRatingCount} évaluations</span>
+                    <span className="text-gray-600 ml-2">
+                      {fixedRatingCount} évaluations
+                    </span>
                   </div>
                   <div className="flex gap-3 justify-center items-end">
                     {product?.sellingPrice === product?.price ? (
