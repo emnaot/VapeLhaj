ba8944cae7d9132c2df422c4dbb01d5f2005def1
Emna Othmen <emna.othmen@gmail.com>
2024-10-15 13:18:59 +0100

    v39

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -11,18 +11,17 @@ import image3Mobile from "../assest/banner/img3_mobile.gif";
 import image4Mobile from "../assest/banner/img4_mobile.gif";
 import image5Mobile from "../assest/banner/img5_mobile.gif";
 
-import { FaAngleRight, FaAngleLeft, FaPause, FaPlay } from "react-icons/fa"; // Importing play and pause icons
-import { useNavigate } from "react-router-dom"; // Utilisation de useNavigate pour la redirection
+import { FaAngleRight, FaAngleLeft, FaPause, FaPlay } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const BannerProduct = () => {
   const [currentImage, setCurrentImage] = useState(0);
-  const [paused, setPaused] = useState(false); // State to control pause/play
-  const navigate = useNavigate(); // Hook pour gérer la redirection
+  const [paused, setPaused] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  // Images pour la version desktop
+  const navigate = useNavigate();
+
   const desktopImages = [image1, image2, image3, image4, image5];
-
-  // Images pour la version mobile
   const mobileImages = [
     image1Mobile,
     image2Mobile,
@@ -31,7 +30,6 @@ const BannerProduct = () => {
     image5Mobile,
   ];
 
-  // Texte attirant pour chaque image
   const bannerTexts = [
     "Découvrez les dernières tendances Vape",
     "Vapoteurs, obtenez vos puffs aujourd'hui",
@@ -40,113 +38,95 @@ const BannerProduct = () => {
     "Vapez avec style et performance",
   ];
 
-  // Catégories associées à chaque banner
-  const bannerCategories = [
-    "pod", // Pour "Découvrez les dernières tendances Vape"
-    "puff", // Pour "Vapoteurs, obtenez vos puffs aujourd'hui"
-    "e-liquide", // Pour "Explorez notre collection de e-liquides"
-    "kit-complet", // Pour "Le meilleur matériel Vape disponible"
-    "colis", // Pour "Vapez avec style et performance"
-  ];
+  const bannerCategories = ["pod", "puff", "e-liquide", "kit-complet", "colis"];
 
-  // Fonction pour passer à l'image suivante
   const nextImage = () => {
-    if (currentImage < desktopImages.length - 1) {
-      setCurrentImage((prev) => prev + 1);
-    } else {
-      setCurrentImage(0); // Retourne à la première image
-    }
+    setCurrentImage((prev) => (prev < desktopImages.length - 1 ? prev + 1 : 0));
   };
 
-  // Fonction pour passer à l'image précédente
   const prevImage = () => {
-    if (currentImage > 0) {
-      setCurrentImage((prev) => prev - 1);
-    } else {
-      setCurrentImage(desktopImages.length - 1); // Retourne à la dernière image
-    }
+    setCurrentImage((prev) => (prev > 0 ? prev - 1 : desktopImages.length - 1));
   };
 
-  // Change l'image automatiquement toutes les 5 secondes
   useEffect(() => {
     if (!paused) {
-      const interval = setInterval(() => {
-        nextImage();
-      }, 5000);
-
-      return () => clearInterval(interval); // Nettoyage de l'intervalle
+      const interval = setInterval(nextImage, 5000);
+      return () => clearInterval(interval);
     }
   }, [currentImage, paused]);
 
-  // Fonction pour rediriger vers la catégorie correspondant à l'image actuelle
   const handleBuyClick = () => {
-    const category = bannerCategories[currentImage]; // Catégorie associée à l'image
-    navigate(`/product-category?category=${category}`); // Redirige vers la page de catégorie
+    navigate(`/product-category?category=${bannerCategories[currentImage]}`);
   };
 
-  // Toggle pause/play
   const togglePause = () => {
     setPaused(!paused);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="mx-auto px-4 md:px-0 max-w-[1350px] mt-5">
-      <div className="h-72 md:h-[480px] w-full bg-slate-200 relative rounded-2xl group">
-        {" "}
-        {/* Hauteur augmentée ici */}
-        {/* Boutons de navigation gauche et droite qui s'affichent uniquement au survol */}
+      <div
+        className={`w-full relative rounded-2xl group ${
+          isMobile ? "h-[400px]" : "h-[480px]"
+        }`}
+      >
         <button
           onClick={prevImage}
           className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-5 shadow-lg hover:bg-gray-200 z-30 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
           style={{ backgroundColor: "#ffffff80" }}
         >
-          <FaAngleLeft className="text-black text-base" />{" "}
+          <FaAngleLeft className="text-black text-base" />
         </button>
         <button
           onClick={nextImage}
           className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white rounded-full p-5 shadow-lg hover:bg-gray-200 z-30 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
           style={{ backgroundColor: "#ffffff80" }}
         >
-          <FaAngleRight className="text-black text-base" />{" "}
+          <FaAngleRight className="text-black text-base" />
         </button>
-
-        {/* Button to pause/play the slideshow */}
         <button
           onClick={togglePause}
           className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white rounded-full p-3 shadow-lg hover:bg-gray-200 z-30 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
           style={{ backgroundColor: "#ffffff80" }}
         >
-          {paused ? <FaPlay className="text-black text-base" /> : <FaPause className="text-black text-base" />}
+          {paused ? (
+            <FaPlay className="text-black text-base" />
+          ) : (
+            <FaPause className="text-black text-base" />
+          )}
         </button>
-
-        {/* Overlay avec texte attirant et bouton */}
         <div className="absolute inset-0 z-20 flex flex-col justify-center items-center text-center text-white p-4">
           <h2 className="text-sm md:text-2xl font-semibold mb-4">
             {bannerTexts[currentImage]}
           </h2>
           <button
-            onClick={handleBuyClick} // Redirection lorsque le bouton est cliqué
+            onClick={handleBuyClick}
             className="text-white px-4 py-2 border border-white rounded-lg hover:bg-white hover:text-black transition-all bg-transparent"
           >
             Acheter &nbsp; <span>&#8250;</span>
           </button>
         </div>
-
-        {/* Version desktop et tablette - Affichage d'une seule image */}
-        <div className="hidden md:flex h-full w-full overflow-hidden rounded-2xl">
+        <div
+          className={`flex ${
+            isMobile ? "h-[400px]" : "hidden md:flex h-full"
+          } w-full overflow-hidden rounded-2xl`}
+        >
           <img
-            src={desktopImages[currentImage]}
+            src={
+              isMobile
+                ? mobileImages[currentImage]
+                : desktopImages[currentImage]
+            }
             className="w-full h-full object-cover rounded-2xl transition-all duration-500 z-10"
             alt={`Banner ${currentImage}`}
-          />
-        </div>
-
-        {/* Version mobile - Affichage d'une seule image */}
-        <div className="flex md:hidden h-full w-full overflow-hidden rounded-2xl">
-          <img
-            src={mobileImages[currentImage]}
-            className="w-full h-full object-cover rounded-2xl transition-all duration-500 z-10"
-            alt={`Banner mobile ${currentImage}`}
           />
         </div>
       </div>
--- frontend/src/components/ProductGrid.js
+++ frontend/src/components/ProductGrid.js
@@ -1,29 +1,20 @@
 import React, { useEffect, useState } from "react";
-import SummaryApi from "../common"; // Pour charger les catégories
-import { Link } from "react-router-dom"; // Pour la redirection
+import SummaryApi from "../common";
+import { Link } from "react-router-dom";
 
-// Importing local images
-import image1 from "../assest/banner/Vozol.webp"; // Change the name to the actual file name
-import image2 from "../assest/banner/drag.jpg"; // Change the name to the actual file name
+import image1 from "../assest/banner/Vozol.webp";
+import image2 from "../assest/banner/drag.jpg";
 
-// Composant pour afficher chaque carte avec une image, un titre et un bouton
 const CardImage = ({ imageUrl, title, buttonText, category }) => {
   return (
-    <div 
-      className="relative w-full h-64 md:h-80 rounded-2xl overflow-hidden shadow-lg transform transition-all duration-300 hover:scale-105 cursor-pointer"
-    >
-      {/* Image */}
-      <img src={imageUrl} alt={title} className="w-full h-full object-cover" />
-
-      {/* Overlay avec titre et bouton */}
+    <div className="relative w-full h-[250px] md:h-80 rounded-2xl overflow-hidden shadow-lg cursor-pointer">
+      <div className="w-full h-full overflow-hidden">
+        <img src={imageUrl} alt={title} className="w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110" />
+      </div>
       <div className="absolute inset-0 bg-black bg-opacity-10 flex flex-col justify-center items-center text-center text-white p-4">
         <h2 className="text-lg md:text-2xl font-semibold mb-4">{title}</h2>
-        <Link 
-          to={`/product-category?category=${category}`} // Redirection dynamique vers la catégorie
-          className="px-4 py-2 rounded-2xl transition-all text-white"
-        >
-          <span className="hover:underline"> {buttonText} </span> {/* Underline only on hover */}
-          &nbsp; <span>&#8250;</span> {/* No underline here */}
+        <Link to={`/product-category?category=${category}`} className="px-4 py-2 rounded-2xl transition-all text-white">
+          <span className="hover:underline">{buttonText}</span> &nbsp; <span>&#8250;</span>
         </Link>
       </div>
     </div>
@@ -32,53 +23,34 @@ const CardImage = ({ imageUrl, title, buttonText, category }) => {
 
 const ProductGrid = () => {
   const [categoryProduct, setCategoryProduct] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768); // Détection mobile
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
-  // Fonction pour récupérer les catégories à partir de l'API
   const fetchCategoryProduct = async () => {
-    setLoading(true);
     const response = await fetch(SummaryApi.categoryProduct.url);
     const dataResponse = await response.json();
-    setLoading(false);
     setCategoryProduct(dataResponse.data);
   };
 
-  // Mettre à jour l'état mobile lors du redimensionnement de la fenêtre
   useEffect(() => {
+    fetchCategoryProduct();
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // Mobile si la largeur de la fenêtre est ≤ 768px
+      setIsMobile(window.innerWidth <= 768);
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  useEffect(() => {
-    fetchCategoryProduct(); // Charger les catégories lors du montage du composant
-  }, []);
-
   return (
     <div className="mx-auto px-4 md:px-0 max-w-[1350px] py-3">
       {isMobile ? (
-        // Version mobile: Afficher les deux cartes côte à côte avec un défilement horizontal
         <div className="flex overflow-x-scroll no-scrollbar space-x-4">
           {categoryProduct.length > 0 ? (
             <>
-              <div className="min-w-[80%]">
-                <CardImage 
-                  imageUrl={image1} // Local image for Vozol Vista
-                  title="Vozol Vista 16000" 
-                  buttonText="ACHETER" 
-                  category="puff" // Redirection vers "puff"
-                />
+              <div className="min-w-[100%]">
+                <CardImage imageUrl={image1} title="Vozol Vista 16000" buttonText="ACHETER" category="puff" />
               </div>
-              <div className="min-w-[80%]">
-                <CardImage 
-                  imageUrl={image2} // Local image for Drag X
-                  title="Pod mod - Drag X" 
-                  buttonText="ACHETER" 
-                  category="pod" // Redirection vers "pod"
-                />
+              <div className="min-w-[100%]">
+                <CardImage imageUrl={image2} title="Pod mod - Drag X" buttonText="ACHETER" category="pod" />
               </div>
             </>
           ) : (
@@ -89,22 +61,11 @@ const ProductGrid = () => {
           )}
         </div>
       ) : (
-        // Version desktop: Afficher les deux cartes en grille
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {categoryProduct.length > 0 ? (
             <>
-              <CardImage 
-                imageUrl={image1} // Local image for Vozol Vista
-                title="Vozol Vista 16000" 
-                buttonText="ACHETER" 
-                category="puff" // Redirection vers "puff"
-              />
-              <CardImage 
-                imageUrl={image2} // Local image for Drag X
-                title="Pod mod - Drag X" 
-                buttonText="ACHETER" 
-                category="pod" // Redirection vers "pod"
-              />
+              <CardImage imageUrl={image1} title="Vozol Vista 16000" buttonText="ACHETER" category="puff" />
+              <CardImage imageUrl={image2} title="Pod mod - Drag X" buttonText="ACHETER" category="pod" />
             </>
           ) : (
             <>
