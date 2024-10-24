7c9637ffcf8c33358004fb1e2a3c609d04b06024
Emna Othmen <emna.othmen@gmail.com>
2024-10-24 10:02:10 +0100

    v63

--- frontend/src/components/FloatingSidebar.js
+++ frontend/src/components/FloatingSidebar.js
@@ -3,13 +3,16 @@ import { FaFacebookF, FaInstagram, FaYoutube, FaTiktok, FaPinterest } from 'reac
 
 const FloatingSidebar = () => {
   return (
-    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 z-50">
-      <div className="flex flex-col items-center space-y-4 p-2 bg-white shadow-lg rounded-full border border-gray-200"
+    <div 
+      className="hidden md:block fixed left-[10px] top-1/2 transform -translate-y-1/2 z-50" // Hidden on mobile, visible on medium and larger screens
+    >
+      <div className="flex flex-col items-center space-y-4 p-2 shadow-lg rounded-full border border-gray-200"
         style={{
           width: '40px', // Reduce the width of the sidebar
-          height: '280px', // Increase the height of the sidebar
+          height: '260px', // Increase the height of the sidebar
           paddingTop: '20px',
           paddingBottom: '20px',
+          backgroundColor: 'rgba(255, 255, 255, 0.7)', // Transparent background for the sidebar
         }}>
         
         {/* Facebook Icon */}
@@ -52,27 +55,24 @@ const FloatingSidebar = () => {
           <FaYoutube size={16} />
         </a>
 
-      
-{/* Vertical Text - 10% DE RÉDUCTION */}
-<div className="mt-4">
-  <div
-    className="text-center font-semibold text-gray-700 px-4 py-2 bg-gray-200 border border-gray-200 rounded-full"
-    style={{ 
-      transform: 'rotate(270deg)', 
-      whiteSpace: 'nowrap',
-      width: '120px', // Adjusted width
-      textAlign: 'center',
-      fontSize: '10px', // Adjust font size to match the style
-      marginTop: '40px', // This will push the element lower from the top
-      height: '30px', // You can also control height directly if needed
-
-    }}
-  >
-    SUIVEZ-NOUS
-    </div>
-</div>
-
-
+        {/* Vertical Text - SUIVEZ-NOUS */}
+        <div className="mt-4">
+          <div
+            className="text-center font-semibold text-gray-700 px-4 py-2 border border-gray-200 rounded-full"
+            style={{ 
+              transform: 'rotate(270deg)', 
+              whiteSpace: 'nowrap',
+              width: '100px', // Adjusted width
+              textAlign: 'center',
+              fontSize: '10px', // Adjust font size to match the style
+              marginTop: '40px', // This will push the element lower from the top
+              height: '30px', // You can also control height directly if needed
+              backgroundColor: 'rgba(230, 230, 230, 0.7)', // Transparent background for the text container as well
+            }}
+          >
+            SUIVEZ-NOUS
+          </div>
+        </div>
       </div>
     </div>
   );
